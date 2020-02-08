32815e50dd1b03c39f2f0c74803ddd5eea49a67a
Javier <javelie@outlook.com>
2020-02-08 08:51:43 -0700

    Pod Install, Realm DB

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '10.0'
+
+target 'BalancedEx' do
+  use_frameworks!
+
+  # Pods for BalancedEx
+	pod 'RealmSwift'
+
+end
---
+++ Pods/Target Support Files/Pods-BalancedEx/Pods-BalancedEx-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BalancedEx : NSObject
+@end
+@implementation PodsDummy_Pods_BalancedEx
+@end
